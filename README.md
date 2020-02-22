7b5ef4decbb7159e28a40a5aa3607d3d42671056
Lucas Löffel <lucas@loeffel.io>
2020-02-22 20:52:12 +0100

    add readme

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # ls-lint
 
-File and directory name linter
+File and Directory name linter
 
 [![Build Status](http://ci.loeffel.io/api/badges/loeffel-io/ls-lint/status.svg)](http://ci.loeffel.io/loeffel-io/ls-lint)
  
+## Example
+
+```yaml
+ls:
+  src:
+    .dir: lowercase
+    .js: snake_case
+    .json: snake_case
+    .vue: PascalCase
+```
+
+
--- linter.go
+++ linter.go
@@ -77,7 +77,7 @@ func (linter *Linter) validateDir(config *Config, index index, path string) erro
 func (linter *Linter) validateFile(config *Config, index index, entrypoint string, path string) error {
 	var g = new(errgroup.Group)
 	var errRules = make([]Rule, 0)
-	
+
 	ext := filepath.Ext(path)
 	rules := config.getConfig(index, path)
 	withoutExt := strings.TrimSuffix(filepath.Base(path), ext)
