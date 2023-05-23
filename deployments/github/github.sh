5184edf1e3a3943c50302809dec8a35e32060484
Lucas Löffel <lucas@loeffel.io>
2023-05-23 16:44:22 +0200

    feat: ready for release

--- deployments/github/github.sh
+++ deployments/github/github.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 gh=$1
 github_files=$2
 
-$gh release create --prerelease --generate-notes --latest ${STABLE_GIT_TAG} $github_files
+$gh release create --generate-notes --latest ${STABLE_GIT_TAG} $github_files
