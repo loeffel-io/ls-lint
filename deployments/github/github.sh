9bfa6f157b49ad3fc1f788f62ab50c51c00ef404
Lucas Löffel <lucas@loeffel.io>
2024-07-15 11:50:46 +0200

    test: prerelease

--- deployments/github/github.sh
+++ deployments/github/github.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 gh=$1
 github_files=$2
 
-$gh release create --generate-notes --prerelease $github_files # --draft # --latest ${STABLE_GIT_TAG}
+$gh release create --generate-notes --prerelease ${STABLE_GIT_TAG} $github_files # --draft # --prerelease/--latest # --latest
