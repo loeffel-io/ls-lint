8d2944a982b013ac66dd832184727fe937a5301b
Lucas Löffel <lucas@loeffel.io>
2024-07-15 12:08:04 +0200

    test: new npm

--- deployments/github/github.sh
+++ deployments/github/github.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 gh=$1
 github_files=$2
 
-$gh release create --generate-notes --prerelease ${STABLE_GIT_TAG} $github_files # --draft # --prerelease/--latest # --latest
+$gh release create --generate-notes --prerelease ${STABLE_GIT_TAG} $github_files # --draft/--prerelease/--latest
